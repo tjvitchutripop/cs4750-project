22a907eb46cf82d4bcb3f2a8beb869a08ef5d931
Yoon <86170304+leeonthe@users.noreply.github.com>
2024-04-27 23:33:28 -0400

    initial commit, working on login / account page

--- header.php
+++ header.php
@@ -13,35 +13,15 @@
                otherwise, display sign up and log in buttons -->
           <?php if (!isset($_SESSION['username'])) { ?>              
             <li class="nav-item">
-              <a class="nav-link" href="register.php">Join our community</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="signin.php">Sign in</a>
+              <a class="nav-link" href="login.php">Log In / Sign up</a>
             </li>              
           <?php  } else { ?>                    
             <li class="nav-item">                  
-              <a class="nav-link" href="signout.php">Sign out</a>
+              <a class="nav-link" href="account.php">Account</a>
             </li>
           <?php } ?>
         
-          <li class="nav-item">
-            <a class="nav-link" href="#">Software</a>
-          </li>            
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" role="button" data-bs-toggle="dropdown" aria-expanded="false">Research</a>
-            <ul class="dropdown-menu" aria-labelledby="dropdown01">
-              <li><a class="dropdown-item" href="#">Design</a></li>
-              <li><a class="dropdown-item" href="#">Development</a></li>
-              <li><a class="dropdown-item" href="#">Testing</a></li>
-              <li><a class="dropdown-item" href="#">Maintenance</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Activity</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
+          
         </ul>
       </div>
     </div>
