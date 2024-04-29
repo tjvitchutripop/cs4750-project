649ac12404fea1999779ab96d6fb9b0845743878
tjvitchutripop <khunteeratham@gmail.com>
2024-04-29 05:10:08 -0400

    Fixed typo in signup.php

--- signup.php
+++ signup.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Litreary Loop | Sign Up</title>
+  <title>Literary Loop | Sign Up</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="styles/main.css">
